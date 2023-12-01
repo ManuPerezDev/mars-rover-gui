25042031d93de871397d530b45ee45ed017a87c3
Manuel Pérez <manuel.perez@collaborator.audiense.com>
2023-12-01 15:36:57 +0000

    feat(RoverLayout): handle a list of commands

--- src/front-end/components/RoverLayout.tsx
+++ src/front-end/components/RoverLayout.tsx
@@ -1,5 +1,5 @@
 import './Rover.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Rover } from '../../mars-rover/src/domain/Rover'
 import GridItems from './GridItems'
 import { MoveForward } from '../../mars-rover/src/domain/MoveForward'
@@ -10,48 +10,62 @@ import { TurnLeft } from '../../mars-rover/src/domain/TurnLeft'
 import { TurnRight } from '../../mars-rover/src/domain/TurnRight'
 
 const RoverLayout = ({ rover }: { rover: Rover }) => {
-  const onClickLeft = () => {
+  const [commands, setCommands] = useState('')
+
+  const onClickLeft = async () => {
     const roverStates = rover.readCommands([new TurnLeft()])
-    drawRover(roverStates)
+    await drawRover(roverStates)
   }
 
-  const onClickRight = () => {
+  const onClickRight = async () => {
     const roverStates = rover.readCommands([new TurnRight()])
-    drawRover(roverStates)
+    await drawRover(roverStates)
   }
 
-  const onClickForward = () => {
+  const onClickForward = async () => {
     const cell = document.getElementById(`${rover.getPosition().getX()},${rover.getPosition().getY()}`)
     cell!.style.backgroundColor = 'white'
     cell!.style.borderColor = 'black'
     const roverStates = rover.readCommands([new MoveForward()])
-    drawRover(roverStates)
+    await drawRover(roverStates)
   }
 
-  const onClickBackward = () => {
+  const onClickBackward = async () => {
     const cell = document.getElementById(`${rover.getPosition().getX()},${rover.getPosition().getY()}`)
     cell!.style.backgroundColor = 'white'
     cell!.style.borderColor = 'black'
     const roverStates = rover.readCommands([new MoveBackward()])
-    drawRover(roverStates)
+    await drawRover(roverStates)
   }
 
-  function drawRover(roverStates: { position: Position, direction: Direction }[]) {
-    const roverFollowingState = roverStates[1]
-    const cell = document.getElementById(`${roverFollowingState.position.getX()},${roverFollowingState.position.getY()}`)
-    cell!.style.backgroundColor = '#F38484'
-    if (roverFollowingState.direction.isNorth()) {
-      cell!.style.borderColor = '#F38484'
-      cell!.style.borderTopColor = 'green'
-    } else if (roverFollowingState.direction.isEast()) {
-      cell!.style.borderColor = '#F38484'
-      cell!.style.borderRightColor = 'green'
-    } else if (roverFollowingState.direction.isSouth()) {
-      cell!.style.borderColor = '#F38484'
-      cell!.style.borderBottomColor = 'green'
-    } else if (roverFollowingState.direction.isWest()) {
-      cell!.style.borderColor = '#F38484'
-      cell!.style.borderLeftColor = 'green'
+  async function drawRover(roverStates: { position: Position, direction: Direction }[]) {
+    const [initialState, ...states] = roverStates
+    const cell = document.getElementById(`${initialState?.position.getX()},${initialState?.position.getY()}`)
+    cell!.style.backgroundColor = 'white'
+    cell!.style.borderColor = 'black'
+
+    for (const [index, state] of states.entries()) {
+      if (index !== 0) {
+        const cell = document.getElementById(`${states[index - 1]?.position.getX()},${states[index - 1]?.position.getY()}`)
+        cell!.style.backgroundColor = 'white'
+        cell!.style.borderColor = 'black'
+      }
+
+      const cell = document.getElementById(`${state.position.getX()},${state.position.getY()}`)
+      cell!.style.backgroundColor = '#F38484'
+      if (state.direction.isNorth()) {
+        cell!.style.borderColor = '#F38484'
+        cell!.style.borderTopColor = 'green'
+      } else if (state.direction.isEast()) {
+        cell!.style.borderColor = '#F38484'
+        cell!.style.borderRightColor = 'green'
+      } else if (state.direction.isSouth()) {
+        cell!.style.borderColor = '#F38484'
+        cell!.style.borderBottomColor = 'green'
+      } else if (state.direction.isWest()) {
+        cell!.style.borderColor = '#F38484'
+        cell!.style.borderLeftColor = 'green'
+      }
     }
   }
 
@@ -77,6 +91,34 @@ const RoverLayout = ({ rover }: { rover: Rover }) => {
     drawRoverFromInitialPosition()
   }, [])
 
+  async function handleSubmit(event: any) {
+    if (commands === '') {
+      return
+    }
+
+    const domainCommands = commands.split('').map(command => {
+      if (command === 'f') {
+        return new MoveForward()
+      } else if (command === 'b') {
+        return new MoveBackward()
+      } else if (command === 'l') {
+        return new TurnLeft()
+      } else if (command === 'r') {
+        return new TurnRight()
+      } else {
+        throw new Error('Invalid command')
+      }
+    })
+
+    const roverStates = rover.readCommands(domainCommands)
+    await drawRover(roverStates)
+    event.preventDefault()
+  }
+
+  function handleChange(event: any) {
+    setCommands(event.target.value)
+  }
+
   return (
     <div>
       <div className='grid-container'>
@@ -86,6 +128,13 @@ const RoverLayout = ({ rover }: { rover: Rover }) => {
       <button onClick={onClickBackward}>Backward</button>
       <button onClick={onClickLeft}>Left</button>
       <button onClick={onClickRight}>Right</button>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Commands:
+          <input type='text' value={commands} onChange={handleChange}/>
+        </label>
+        <input type='submit' value='Submit'/>
+      </form>
     </div>
   )
 }
